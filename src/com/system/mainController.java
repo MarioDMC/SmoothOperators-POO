df3998970ab0f71029658b5ba36cdef106dd242b
MarioDMC <MarioDMC@users.noreply.github.com>
2019-01-29 23:58:57 -0500

    Commit Inicial
    Archivos del proyecto Spring

---
+++ src/com/system/mainController.java
@@ -0,0 +1,14 @@
+package com.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "main-menu";
+
+    }
+}
